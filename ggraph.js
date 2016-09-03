3ad183320c209d653e3d179224bf221e9bd510a6
Gordon Woodhull <gordon@woodhull.com>
2016-09-03 16:17:55 -0400

    a simple graph adt
    now, what am i really trying to do?

--- ggraph.js
+++ ggraph.js
@@ -10,21 +10,39 @@ function ggraph(nodes, edges, options) {
 
     var _nodeIndex, _edgeIndex, _outsList, _insList;
 
-    function build_index(vs, acc) {
+    function build_index(vs, acc, wrap) {
         return vs.reduce(function(o, v) {
-            o[acc(v)] = v;
+            o[acc(v)] = wrap(v);
             return o;
         }, {});
     }
     function build_node_index() {
         if(_nodeIndex)
             return;
-        _nodeIndex = build_index(nodes, options.nodeKey);
+        _nodeIndex = build_index(nodes, options.nodeKey, node_wrapper);
     }
     function build_edge_index() {
         if(_edgeIndex)
             return;
-        _edgeIndex = build_index(edges, options.edgeKey);
+        _edgeIndex = build_index(edges, options.edgeKey, edge_wrapper);
+    }
+    function build_edge_list(acc) {
+        build_edge_index();
+        return edges.reduce(function(o, v) {
+            var l = o[acc(v)] = o[acc(v)] || [];
+            l.push(_graph.edge(options.edgeKey(v)));
+            return o;
+        }, {});
+    }
+    function build_outs_index() {
+        if(_outsList)
+            return;
+        _outsList = build_edge_list(options.edgeSource);
+    }
+    function build_ins_index() {
+        if(_insList)
+            return;
+        _insList = build_edge_list(options.edgeTarget);
     }
     function node_wrapper(n) {
         return {
@@ -33,10 +51,34 @@ function ggraph(nodes, edges, options) {
             },
             name: function() {
                 return options.nodeKey(n);
+            },
+            outs: function() {
+                build_outs_index();
+                return _outsList[options.nodeKey(n)];
+            },
+            ins: function() {
+                build_ins_index();
+                return _insList[options.nodeKey(n)];
             }
         };
     }
-    return {
+    function edge_wrapper(e) {
+        return {
+            value: function() {
+                return e;
+            },
+            name: function() {
+                return options.edgeKey(e);
+            },
+            source: function() {
+                return _graph.node(options.edgeSource(e));
+            },
+            target: function() {
+                return _graph.node(options.edgeTarget(e));
+            }
+        };
+    }
+    var _graph = {
         node: function(key) {
             build_node_index();
             return _nodeIndex[key];
@@ -46,5 +88,6 @@ function ggraph(nodes, edges, options) {
             return _edgeIndex[key];
         }
     };
+    return _graph;
 }
 
