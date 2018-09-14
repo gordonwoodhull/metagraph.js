e7b4df43d6934c16fb44513a6ebc2f889d52e860
Gordon Woodhull <gordon@woodhull.com>
2018-09-14 10:14:12 -0400

    instantiation of dataflow should be separate step
    currying, sort of... but i really like to think in terms of a sort of asynchronous continuation with
    context mmhrrm

--- src/dataflow.js
+++ src/dataflow.js
@@ -1,14 +1,19 @@
 metagraph.dataflow = function(spec, options) {
     var flowgraph = mg.graph(spec.nodes, spec.edges, options);
     var _flow = {
-        calc: function(instance, id) {
-            if(!instance[id]) {
-                var n = flowgraph.node(id);
-                instance[id] = n.value().calc.apply(null, n.ins().map(function(e) {
-                    return _flow.calc(instance, e.source().key());
-                }));
-            }
-            return instance[id];
+        create: function(instance) {
+            var _inst = {
+                calc: function(id) {
+                    if(!instance[id]) {
+                        var n = flowgraph.node(id);
+                        instance[id] = n.value().calc.apply(null, n.ins().map(function(e) {
+                            return _inst.calc(e.source().key());
+                        }));
+                    }
+                    return instance[id];
+                }
+            };
+            return _inst;
         }
     };
     return _flow;
